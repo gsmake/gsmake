b5e9d90cf754f43e38e12b8153a946f6226048c5
yayanyang <yayanyang@gmail.com>
2016-01-15 02:12:55 +0800

    update ADL bug for gcc

--- lemon/src/main/cpp/lemon/os/os_errors.hpp
+++ lemon/src/main/cpp/lemon/os/os_errors.hpp
@@ -3,57 +3,60 @@
 #include <system_error>
 
 namespace lemon{ namespace os{
-	enum class errc 
-	{
-		command_not_found
-	};
+        enum class errc
+        {
+            command_not_found
+        };
 
-	class os_error_category : public std::error_category
-	{
-	private:
+        class os_error_category : public std::error_category
+        {
+        private:
 
-		const char *name() const throw()
-		{
-			return "lemon::os::os_error_category";
-		}
+            const char *name() const throw()
+            {
+                return "lemon::os::os_error_category";
+            }
 
-		std::string message(int _Errval) const
-		{
-			switch ((errc)_Errval)
-			{
-			case errc::command_not_found:
-				return "command execute not found";
-			default:
-				return "unknown error code";
-			}
-		}
+            std::string message(int _Errval) const
+            {
+                switch ((errc)_Errval)
+                {
+                case errc::command_not_found:
+                    return "command execute not found";
+                }
+            }
 
-		std::error_condition default_error_condition(int _Errval) const throw()
-		{
-			return std::error_condition(_Errval,*this);
-		}
+            std::error_condition default_error_condition(int _Errval) const throw()
+            {
+                return std::error_condition(_Errval,*this);
+            }
 
-		bool equivalent(int _Errval,const std::error_condition& _Cond) const throw()
-		{
-			return _Errval == _Cond.value();
-		}
+            bool equivalent(int _Errval,const std::error_condition& _Cond) const throw()
+            {
+                return _Errval == _Cond.value();
+            }
 
-		bool equivalent(const std::error_code& _Code, int _Errval) const throw()
-		{
-			return _Errval == _Code.value();
-		}
-	};
+            bool equivalent(const std::error_code& _Code, int _Errval) const throw()
+            {
+                return _Errval == _Code.value();
+            }
+        };
 
+        inline std::error_code is_error_code_enum(lemon::os::errc err)
+        {
+            static os_error_category os_error_category;
 
-}}
+            return std::error_code(static_cast<int>(err), os_error_category);
+        }
+    }}
 
 namespace std{
-	template<> struct is_error_code_enum<lemon::os::errc> : true_type {};
+    template<> struct is_error_code_enum<lemon::os::errc> : true_type {};
 
-	/*inline std::error_condition make_error_condition(lemon::os::errc err)
-	{
-		return err
-	}*/
+    /*inline std::error_condition make_error_condition(lemon::os::errc err)
+    {
+        return err
+    }*/
 }
 
 #endif //LEMON_OS_ERRORS_HPP
